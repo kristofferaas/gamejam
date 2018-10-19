1915c0b2d8ecaf8ae765c2688d73ce2d8d6c9988
Kristoffer Aas <mail@kristoffer.io>
2018-10-20 00:13:49 +0200

    added range

---
+++ Assets/Scripts/Bouncer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+
+    public float throwForce;
+    public Canvas range;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        print(range);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+}
