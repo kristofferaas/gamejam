5bd9de3bda7fb2b5894172a86b6ecb7eefde0442
Christian Echtermeyer <chrech.birr@gmail.com>
2018-10-20 17:13:35 +0200

    added two more collidable objects and fixed some minor number errors

--- Assets/Scripts/GraphicsSpawner.cs
+++ Assets/Scripts/GraphicsSpawner.cs
@@ -24,31 +24,43 @@ public class GraphicsSpawner : MonoBehaviour {
 
             float randomY = Random.Range(-15, 15);
 
-            if (height < 100 && rb.velocity.magnitude > 20)
+            float positionY = gameObject.transform.position.y + randomY + rb.velocity.y;
+            float positionX = gameObject.transform.position.x + rb.velocity.x;
+
+            if (positionY < 0.5f)
+            {
+                positionY = 0.5f;
+            }
+
+            GameObject spawn;
+
+            if (height < 100 && rb.velocity.x > 10)
             {
                 if (roll > 20)
                 {
-                    GameObject spawn = cloudPool.GetPooledObject();
-                    spawn.transform.position = new Vector3(gameObject.transform.position.x + rb.velocity.x, gameObject.transform.position.y + randomY + rb.velocity.y, 50);
+                    spawn = cloudPool.GetPooledObject();
+                    spawn.transform.position = new Vector3(positionX, positionY, 50);
                     spawn.SetActive(true);
                 }
             } else if (height < 300 && rb.velocity.magnitude > 20)
             {
                 if (roll > 95)
                 {
-                    GameObject spawn = planePool.GetPooledObject();
-                    spawn.transform.position = new Vector3(gameObject.transform.position.x + rb.velocity.x, gameObject.transform.position.y + randomY + rb.velocity.y, 50);
+                    spawn = planePool.GetPooledObject();
+                    spawn.transform.position = new Vector3(positionX, positionY, 50);
                     spawn.SetActive(true);
                 }
             } else if (rb.velocity.magnitude > 20)
             {
                 if (roll > 95)
                 {
-                    GameObject spawn = sattelitePool.GetPooledObject();
-                    spawn.transform.position = new Vector3(gameObject.transform.position.x + rb.velocity.x, gameObject.transform.position.y + randomY + rb.velocity.y, 50);
+                    spawn = sattelitePool.GetPooledObject();
+                    spawn.transform.position = new Vector3(positionX, positionY, 50);
                     spawn.SetActive(true);
                 }
             }
+            
+
             spawnTimer = 0.0f;
         }
         spawnTimer += Time.deltaTime;
--- Assets/Scripts/TerrainLoader.cs
+++ Assets/Scripts/TerrainLoader.cs
@@ -22,7 +22,8 @@ public class TerrainLoader : MonoBehaviour
 			SwitchTerrain();
 		}
         float negate = 1 - gameObject.transform.position.y / 200;
-        sky.GetComponent<SpriteRenderer>().color = new Color(0, negate - 0.1f, negate, 1 - negate);
+        float negateAlpha = 1 - gameObject.transform.position.y / 500;
+        sky.GetComponent<SpriteRenderer>().color = new Color(0, negate - 0.1f, negate, 1 - negateAlpha);
         sky.transform.position = gameObject.transform.position + new Vector3(0, 0, 100);
     }
 
