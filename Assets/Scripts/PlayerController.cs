72c365e13cf2bd05510bc9fd1bea99e232dd2c1a
Håkon Paulsen <haakonpaulsen1234@hotmail.com>
2018-10-20 16:14:27 +0200

    Added some hud and boosting.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class PlayerController : MonoBehaviour
 {
 
     Rigidbody2D rb2d;
-    [SerializeField] private float thrust;
-	[SerializeField] private GameObject energyText;
-    bool hasLaunched = false;
-	float energy = 100;
+    public float thrust;
+    public float energy = 100;
+	public GameObject energyText;
+	public GameObject distanceText;
+    public Transform startPos;
 
+    bool hasLaunched = false;
+    float distance = 0;
+	
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
-		updateEnergyText();
+		UpdateEnergyText();
+        UpdateDistanceText();
     }
 
-	private void updateEnergyText()
+	private void UpdateEnergyText()
     {
-        energyText.GetComponent<Text>().text = "Energy: " + energy;
+        energyText.GetComponent<Text>().text = (int) energy + "ml";
+    }
+
+    private void UpdateDistanceText()
+    {
+        distanceText.GetComponent<Text>().text = (int) distance + "m";
     }
 
     // Use this for initialization
@@ -39,23 +50,39 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(hasLaunched) {
+            distance = Vector2.Distance(new Vector2(startPos.position.x, startPos.position.y), new Vector2(transform.position.x, transform.position.y));
+            UpdateDistanceText();
+        }
     }
 
-	private void boost()
+	private void Boost()
     {
 		energy--;
 		rb2d.AddForce(Vector2.up*thrust*0.35f, ForceMode2D.Impulse);
 		rb2d.AddForce(Vector2.right*thrust*0.15f, ForceMode2D.Impulse);
-        updateEnergyText();
+        UpdateEnergyText();
     }
+
+    private void Launch(float forceForward, float forceUpward)
+    {
+        hasLaunched = true;
+		rb2d.AddForce(Vector2.up*forceForward, ForceMode2D.Impulse);
+		rb2d.AddForce(Vector2.right*forceUpward, ForceMode2D.Impulse);
+    }
+
 
     private void FixedUpdate()
     {  
-        if (Input.GetAxis("Jump") == 1 && energy > 0)
+        
+        if (Input.GetAxis("Jump") == 1 && !hasLaunched)
         {
-            boost();
-            hasLaunched = true;
+            Launch(15f, 15f);
+        }
+
+        if (Input.GetAxis("Jump") == 1 && energy > 0 && hasLaunched)
+        {
+            Boost();
         }
         
     }
