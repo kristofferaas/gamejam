4a7a07f3ef3d5a7ca5144b5624d20461b373e01a
Christian Echtermeyer <chrech.birr@gmail.com>
2018-10-20 02:04:12 +0200

    proof of concept for terrain loader

---
+++ Assets/Scripts/DummyPlayerMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyPlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		gameObject.transform.position = gameObject.transform.position + new Vector3(3.0f, 0.0f, 0.0f);
+	}
+}
---
+++ Assets/Scripts/TerrainLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainLoader : MonoBehaviour
+{
+	public GameObject currentTerrain;
+	public GameObject nextTerrain;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log(currentTerrain.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.x > currentTerrain.transform.position.x - 50)
+		{
+			SwitchTerrain();
+		}
+	}
+
+	private void SwitchTerrain()
+	{
+		GameObject save = currentTerrain;
+		currentTerrain = nextTerrain;
+		currentTerrain.transform.position = nextTerrain.transform.position + new Vector3(200.0f, 0.0f, 0.0f);
+		nextTerrain = save;
+	}
+}
