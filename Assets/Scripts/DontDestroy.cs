94616b2e2df5065bbd655cf5f5cc1eeda4ff8bac
Christian Echtermeyer <chrech.birr@gmail.com>
2018-10-21 17:36:58 +0200

    added end scene basic

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroy : MonoBehaviour {
+
+    private static bool created = false;
+
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+            Debug.Log("Awake: " + this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain : MonoBehaviour {
+
+    private float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > 10)
+        {
+            TaskOnClick();
+            timer = 0.0f;
+        }
+	}
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
